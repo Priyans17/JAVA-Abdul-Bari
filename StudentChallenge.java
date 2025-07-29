63b1a4e651fc750863ea5881f2cd967207ccb9f8
Priyans Mehta <120036015+Priyans17@users.noreply.github.com>
2025-07-29 13:11:05 +0530

    Add files via upload

---
+++ StudentChallenge.java
@@ -0,0 +1,94 @@
+package Priyansh;
+
+public class StudentChallenge {
+    //find the email id is on gmail &
+// Find username and domain name from email
+
+//    public static void main(String[] args) {
+//        String str = "programmer@gmail.com";
+//        int i = str.indexOf("@");
+//        String uname = str.substring(0, i);
+//        String domain = str.substring(i+1, str.length());
+//        System.out.println("Username " + uname);
+//        System.out.println("Domain " + domain);
+//
+//        System.out.println(domain.startsWith("gmail"));
+//    }
+//}
+
+    //method 2
+//    public static void main(String[] args) {
+//        String str = "programmer@gmail.com";
+//        int i = str.indexOf("@");
+//        String uname = str.substring(0, i);
+//        String domain = str.substring(i+1, str.length());
+//        System.out.println("Username " + uname);
+//        System.out.println("Domain " + domain);
+//
+//        int j = domain.indexOf(".");
+//        String name = domain.substring(0, j);
+//        System.out.println(name.equals("gmail"));
+//    }
+//}
+//
+
+//Find if number is binary or not
+//
+//    public static void main(String[] args) {
+//        int b = 1001100;
+//        String str = String.valueOf(b);
+//        System.out.println(str.matches("[01]+"));
+//    }
+//}
+
+//Find is a Number Hexadecimal of Not
+//
+//    public static void main(String[] args) {
+//        String str = "aef009";
+//        System.out.println(str.matches("[0-9a-f]+"));
+//    }
+//}
+
+
+//Find is the data in format (dd/mm/yy)
+//
+//    public static void main(String[] args) {
+//        String str = "01/12/2003";
+//        System.out.println(str.matches("[0-3][0-9]/[01][0-9]/[0-9]{4}"));
+//    }
+//}
+
+//Remove special characters from a string
+
+//    public static void main(String[] args) {
+//        String str = "a!B!c#$%2(87";
+//        System.out.println(str.replaceAll("[^a-zA-Z0-9]", ""));
+//    }
+//}
+
+//Remove extra spaces form the String
+
+//    public static void main(String[] args) {
+//        String str = "abc   def   kkkll    eefefenf ";
+//        System.out.println(str.replaceAll("\\s+"," "));
+//    }
+//}
+
+//    public static void main(String[] args) {
+//        String str = "   abc def hij klm nop   ";
+//        System.out.println(str.replaceAll("\\s+", " ").trim());
+//    }
+//}
+
+
+//Find the number of words in a String
+
+
+//public static void main(String[] args) {
+//    String str = " abc jk lmo jef e ejfe fke";
+//    str = str.replaceAll("\\s+", " ").trim();
+//    String words[] = str.split("\\s+");
+//    System.out.println(words.length);
+//
+//}
+}
