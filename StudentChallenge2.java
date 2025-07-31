072ede707a7af80f1666977769999d089cb7e06c
Priyans Mehta <120036015+Priyans17@users.noreply.github.com>
2025-07-31 11:32:08 +0530

    Student Challenge 2

---
+++ StudentChallenge2.java
@@ -0,0 +1,140 @@
+package Priyansh;
+import java.util.Scanner;
+public class StudentChallenge2 {
+
+    public static void main(String[] args) {
+        //Find a number is odd or even
+//        int n;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the number ");
+//        n = sc.nextInt();
+//
+//        if (n%2 == 0) {
+//            System.out.println("Number is Even");
+//        }
+//        else  {
+//            System.out.println("Number is Odd");
+//        }
+
+        //Find person is young or not young
+//        int age;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the Age ");
+//        age = sc.nextInt();
+//        if (age >= 14 && age <= 55) {
+//            System.out.println("You are young");
+//        } else {
+//            System.out.println("You aren't young");
+//        }
+
+        //Find grades for given marks
+//        int m1, m2, m3;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the marks of 3 Subjects :- ");
+//        m1 = sc.nextInt();
+//        m2 = sc.nextInt();
+//        m3 = sc.nextInt();
+//
+//        float avg = (float) (m1+m2+m3)/3;
+//        if(avg>70) {
+//            System.out.println("GRADE A");
+//        } else if (avg >= 60 && avg < 70) {
+//            System.out.println("GRADE B");
+//        } else if (avg >= 50 && avg < 60 ) {
+//            System.out.println("GRADE C");
+//        } else if (avg >= 40 && avg < 50) {
+//            System.out.println("GRADE D");
+//        } else {
+//            System.out.println("GRADE E");
+//        }
+
+        //Find Radix of a number given in a string
+
+//        Scanner sc = new Scanner(System.in);
+//        String num;
+//
+//        System.out.println("Enter the number");
+//        num = sc.nextLine();
+//
+//        if(num.matches("[01]+")) {
+//            System.out.println("Number is Binary Radix = 2");
+//        } else if (num.matches("[0-7]+")) {
+//            System.out.println("Number is Octal Radix =  8");
+//        } else if (num.matches("[0-9]+")){
+//            System.out.println("Number is Decimal Radix  = 10");
+//        } else if (num.matches("[0-9A-F]+")) {
+//            System.out.println("Number is Hexadecimal Radix = 16");
+//        } else {
+//            System.out.println("Invalid Number");
+//        }
+
+        //Find a given year is a leap year or not
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("Enter a Year number");
+//        int year = scan.nextInt();
+//        if (year % 4 == 0) {
+//            if (year % 100 == 0) {
+//                if (year % 400 == 0) {
+//                    System.out.println("Its a Leap Year");
+//                } else
+//                    System.out.println("Not a Leap Year");
+//            } else {
+//                System.out.println("Its a Leap Year");
+//
+//            }
+//        } else {
+//            System.out.println("Not a Leap Year");
+//        }
+
+        //Display the day based on the number
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter the Day number");
+//        int day = sc.nextInt();
+//        if (day == 1) {
+//            System.out.println("Monday");
+//        } else if (day == 2) {
+//            System.out.println("Tuesday");
+//        } else if (day == 3){
+//            System.out.println("Wednesday");
+//        } else if (day == 4) {
+//            System.out.println("Thursday");
+//        } else if (day == 5) {
+//            System.out.println("Friday");
+//        } else if (day == 6) {
+//            System.out.println("Saturday");
+//        } else if (day == 7) {
+//            System.out.println("Sunday");
+//        } else {
+//            System.out.println("Invalid Date Entered");
+//        }
+
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("Enter a URL");
+//        String url = scan.nextLine();
+//
+//        String protocol = url.substring(0, url.indexOf(":"));
+//
+//        if (protocol.equals("http"))
+//            System.out.println("Hypertext Transfer Protocol");
+//        else if (protocol.equals("ftp"))
+//            System.out.println("File Transfer Protocol");
+//
+//
+//        String ext = url.substring(url.lastIndexOf(".") + 1);
+//
+//        if (ext.equals("com"))
+//            System.out.println("Commercial");
+//        else if (ext.equals("org"))
+//            System.out.println("Organisation");
+//        else if (ext.equals("net"))
+//            System.out.println("Network");
+//
+//
+    }
+}
