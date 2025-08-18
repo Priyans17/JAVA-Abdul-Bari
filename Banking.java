1e074b1bf3dab5f8a7fdd22e3a2768485c727dd0
Priyans Mehta <120036015+Priyans17@users.noreply.github.com>
2025-08-18 13:28:18 +0530

    Banking.java

--- Banking.java
+++ Banking.java
@@ -8,6 +8,7 @@ class Account {
     private String dob;
     protected long balance;
 
+    // Parameterized constructor
     public Account(String acc, String n, String add, String phnNo, String dob) {
         accNo = acc;
         name = n;
@@ -17,40 +18,20 @@ class Account {
         balance = 0;
     }
 
-    public String getAccNo() {
-        return accNo;
-    }
+    // Getters
+    public String getAccNo() { return accNo; }
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+    public String getPhnNo() { return phnNo; }
+    public String getDob() { return dob; }
+    public long getBalance() { return balance; }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPhnNo() {
-        return phnNo;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public long getBalance() {
-        return balance;
-    }
-
-    public void setAddress(String add) {
-        address = add;
-    }
-
-    public void setPhnNo(String phnNo) {
-        this.phnNo = phnNo;
-    }
+    // Setters
+    public void setAddress(String add) { address = add; }
+    public void setPhnNo(String phnNo) { this.phnNo = phnNo; }
 }
 
-// Savings Account
+// SavingsAccount class
 class SavingsAccount extends Account {
     public SavingsAccount(String acc, String n, String add, String phnNo, String dob) {
         super(acc, n, add, phnNo, dob);
@@ -61,27 +42,25 @@ class SavingsAccount extends Account {
     }
 
     public void withdraw(long amt) {
-        if (amt <= balance) {
-            balance -= amt; // deduct from balance
-        } else {
+        if (amt <= balance)
+            balance -= amt;
+        else
             System.out.println("Insufficient Balance!");
-        }
     }
 }
 
-// Loan Account
+// LoanAccount class
 class LoanAccount extends Account {
-    public LoanAccount(String acc, String n, String add, String phnNo, String dob, long loanAmount) {
+    public LoanAccount(String acc, String n, String add, String phnNo, String dob, long loanAmt) {
         super(acc, n, add, phnNo, dob);
-        balance = loanAmount; // loan balance = amount to be repaid
+        balance = loanAmt; // loan amount to be repaid
     }
 
     public void payEMI(long amt) {
-        if (amt <= balance) {
+        if (amt <= balance)
             balance -= amt;
-        } else {
-            System.out.println("Extra amount! Loan is already cleared.");
-        }
+        else
+            System.out.println("Extra EMI amount entered!");
     }
 
     public void repay(long amt) {
@@ -89,21 +68,21 @@ class LoanAccount extends Account {
             balance = 0;
             System.out.println("Loan fully repaid!");
         } else {
-            System.out.println("Repay amount must be equal to remaining loan balance!");
+            System.out.println("Repay amount must equal remaining balance!");
         }
     }
 }
 
-// Main Class
+// Main class
 public class Banking {
     public static void main(String[] args) {
-        // Savings Account demo
+        // Create a SavingsAccount
         SavingsAccount sa = new SavingsAccount("A101", "Priyansh", "Mumbai", "9876543210", "01-01-2000");
         sa.deposit(5000);
         sa.withdraw(2000);
         System.out.println("Savings Account Balance: " + sa.getBalance());
 
-        // Loan Account demo
+        // Create a LoanAccount
         LoanAccount la = new LoanAccount("L202", "Mehta", "Delhi", "9123456780", "05-05-1999", 10000);
         la.payEMI(3000);
         System.out.println("Loan Balance after EMI: " + la.getBalance());
