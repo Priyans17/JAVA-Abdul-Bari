c3e355054f6cd74d362b6584401f45f9f85f7a86
Priyans Mehta <120036015+Priyans17@users.noreply.github.com>
2025-08-25 10:12:35 +0530

    Parameterised Constructors

--- inheritconst.java
+++ inheritconst.java
@@ -27,30 +27,31 @@ package MultipleClasses;
 
 //Parameterised Constructors
 
-    class Parent {
-        Parent() {
-            System.out.println("Non Param of Parent");
-        }
-        Parent(int x) {
-            System.out.println("Param of Parent " + x);
-    }
-}
+//    class Parent {
+//        Parent() {
+//            System.out.println("Non Param of Parent");
+//        }
+//        Parent(int x) {
+//            System.out.println("Param of Parent " + x);
+//    }
+//}
+//
+//    class Child extends Parent {
+//        Child() {
+//            System.out.println("Non Param of Child");
+//        }
+//        Child(int y) {
+//            System.out.println("Param of Child");
+//        }
+//        Child(int x, int y){
+//            super(x); //Super is a keyword which refers to the super class
+//            System.out.println("2 Param of Child " + y);
+//        }
+//    }
+//public class inheritconst {
+//    public static void main(String[] args) {
+//        Child c = new Child(10,20);
+//
+//    }
+//}
 
-    class Child extends Parent {
-        Child() {
-            System.out.println("Non Param of Child");
-        }
-        Child(int y) {
-            System.out.println("Param of Child");
-        }
-        Child(int x, int y){
-            super(x); //Super is a keyword which refers to the super class
-            System.out.println("2 Param of Child " + y);
-        }
-    }
-public class inheritconst {
-    public static void main(String[] args) {
-        Child c = new Child(10,20);
-
-    }
-}
